b785c2a5eb8cac99a6c1fb6f92caa0568f5337be
Linus Andersson <mr.andersson.linus@gmail.com>
2016-04-23 18:35:01 +0200

    more prints first change

--- MyProject/src/MyClass.java
+++ MyProject/src/MyClass.java
@@ -4,6 +4,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hej");
+		System.out.println("Hej2");
+		System.out.println("Hej3");
 		
 	}
 
