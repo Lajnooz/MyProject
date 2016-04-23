dcb6310ce95b834f803aba2e807e1867005640df
Bananamike <mikael.motyka@gmail.com>
2016-04-23 18:53:45 +0200

    test2

--- MyProject/src/MyClass.java
+++ MyProject/src/MyClass.java
@@ -4,10 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hej");
-		System.out.println("Hej42");
-		System.out.println("Hej3");
-		System.out.println("Hej3");
-		
 	}
 
 }
