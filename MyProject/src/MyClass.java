f20390fa3175838902d1ba0a0c85e90a2b27c4a1
Bananamike <mikael.motyka@gmail.com>
2016-04-23 19:06:04 +0200

    Välkommen

--- MyProject/src/MyClass.java
+++ MyProject/src/MyClass.java
@@ -4,6 +4,9 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hej");
+		System.out.println("Och");
+		System.out.println("Välkommen");
+		System.out.println("Lajnooz");
 		
 	}
 
